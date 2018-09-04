9fa397e1bd4485896310f33e90605157fb3ea76c
salamri <42224359+salamri@users.noreply.github.com>
2018-09-04 15:00:33 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -7,11 +7,11 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = 'a';
+const prefix = 'c';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`aplay`,"https://www.twitch.tv/Anime & Games")
+client.user.setGame(`cplay`,"https://www.twitch.tv/Anime & Games")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
